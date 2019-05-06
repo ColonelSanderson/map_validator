9b435e4d6d019cb724cd21a12146100f8b1a79f0
Mark Triggs <mark@dishevelled.net>
2019-05-06 14:30:23 +1000

    Little string -> symbol issue on the notifications test

--- tests/notification.test.rb
+++ tests/notification.test.rb
@@ -13,11 +13,11 @@ class TestNotifications < Test::Unit::TestCase
 
   def test_format_source
     assert_raise(RuntimeError, 'Invalid type.') { @notifications.add_notification('CATASTROPHIC_ERROR') }
-    assert_nothing_raised (RuntimeError) { @notifications.add_notification('ERROR') }
-    @notifications.add_notification('WARNING', 'The was a problem, but only a little one.', 'C17')
+    assert_nothing_raised (RuntimeError) { @notifications.add_notification(:ERROR) }
+    @notifications.add_notification(:WARNING, 'The was a problem, but only a little one.', 'C17')
     assert_equal(@notifications.notification_list[1].message, 'The was a problem, but only a little one.')
     assert_equal(@notifications.notification_list[1].source, 'C17')
-    @notifications.add_notification('WARNING', 'The was a noteworthy event, but not necessarily a dangerous one', 'C17');
+    @notifications.add_notification(:WARNING, 'The was a noteworthy event, but not necessarily a dangerous one', 'C17');
     assert_equal(@notifications.notification_list.count, 3)
   end
 
